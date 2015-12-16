018e7c40eb0b2b1497f91a83352186437d0a7302
池田享浩 <ikedatakahiro@rrcs-172-254-99-25.nyc.biz.rr.com>
2015-12-17 05:49:27 +0900

    Initial Commit

---
+++ MapData/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MapData
+//
+//  Created by 池田享浩 on 2015/12/17.
+//  Copyright (c) 2015年 takahiro ikeda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MapData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MapData
+//
+//  Created by 池田享浩 on 2015/12/17.
+//  Copyright (c) 2015年 takahiro ikeda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
